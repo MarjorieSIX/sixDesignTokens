d6c91068e130c74ba2db07b8a6f0f6cb66ba9de5
Marjorie Lazaro <marjorie.lazaro@desjardins.com>
2019-09-18 09:16:43 -0400

    update style-dictionary + ajout de properties

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,33 +3,37 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Mon, 22 Apr 2019 17:31:16 GMT
+// Generated on Tue, 17 Sep 2019 20:19:31 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-ColorBlack,
-ColorBlue30,
-ColorBlue50,
-ColorBlue00,
-ColorGraphite40,
-ColorGraphite50,
-ColorGraphite0010,
-ColorGreen30,
-ColorGreen50,
-ColorGreen00,
-ColorOrange00,
-ColorOrange05,
-ColorOrange4550,
-ColorRed30,
-ColorRed50,
-ColorRed00,
 ColorWhite,
-ColorYellow50,
-ColorYellow00,
-ColorYellow0010
+ColorGrey,
+ColorBlack,
+ColorPrimary,
+ColorBlueLight,
+ColorBlueMedium,
+ColorBlueDark,
+ColorRedLight,
+ColorRedMedium,
+ColorRedDark,
+ColorOrangeLight,
+ColorOrangeMedium,
+ColorOrangeDark,
+ColorYellowLight,
+ColorYellowMedium,
+ColorYellowDark,
+ColorGreenLight,
+ColorGreenMedium,
+ColorGreenDark,
+ColorGraphiteLight,
+ColorGraphiteMedium,
+ColorGraphiteDark,
+ColorAdvantage,
+ColorAccent
 };
 
 @interface StyleDictionaryColor : NSObject
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Mon, 22 Apr 2019 17:31:16 GMT
+// Generated on Tue, 17 Sep 2019 20:19:31 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -21,26 +21,30 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
+[UIColor colorWithRed:1.00f green:1.00f blue:1.00f alpha:1.00f],
+[UIColor colorWithRed:0.67f green:0.67f blue:0.67f alpha:1.00f],
 [UIColor colorWithRed:0.00f green:0.00f blue:0.00f alpha:1.00f],
+[UIColor colorWithRed:0.00f green:0.53f blue:0.31f alpha:1.00f],
+[UIColor colorWithRed:0.89f green:0.87f blue:0.95f alpha:1.00f],
 [UIColor colorWithRed:0.25f green:0.26f blue:0.86f alpha:1.00f],
 [UIColor colorWithRed:0.09f green:0.11f blue:0.44f alpha:1.00f],
-[UIColor colorWithRed:0.89f green:0.87f blue:0.95f alpha:1.00f],
-[UIColor colorWithRed:0.40f green:0.40f blue:0.40f alpha:1.00f],
-[UIColor colorWithRed:0.22f green:0.22f blue:0.22f alpha:1.00f],
-[UIColor colorWithRed:0.90f green:0.91f blue:0.91f alpha:1.00f],
-[UIColor colorWithRed:0.00f green:0.53f blue:0.31f alpha:1.00f],
-[UIColor colorWithRed:0.02f green:0.29f blue:0.18f alpha:1.00f],
-[UIColor colorWithRed:0.80f green:0.91f blue:0.86f alpha:1.00f],
+[UIColor colorWithRed:1.00f green:0.87f blue:0.87f alpha:1.00f],
+[UIColor colorWithRed:0.82f green:0.00f blue:0.14f alpha:1.00f],
+[UIColor colorWithRed:0.47f green:0.01f blue:0.09f alpha:1.00f],
 [UIColor colorWithRed:0.96f green:0.92f blue:0.85f alpha:1.00f],
 [UIColor colorWithRed:0.93f green:0.57f blue:0.04f alpha:1.00f],
 [UIColor colorWithRed:0.25f green:0.16f blue:0.01f alpha:1.00f],
-[UIColor colorWithRed:0.92f green:0.01f blue:0.16f alpha:1.00f],
-[UIColor colorWithRed:0.47f green:0.01f blue:0.09f alpha:1.00f],
-[UIColor colorWithRed:1.00f green:0.78f blue:0.78f alpha:1.00f],
-[UIColor colorWithRed:1.00f green:1.00f blue:1.00f alpha:1.00f],
-[UIColor colorWithRed:0.25f green:0.25f blue:0.01f alpha:1.00f],
 [UIColor colorWithRed:0.95f green:0.96f blue:0.85f alpha:1.00f],
-[UIColor colorWithRed:0.96f green:0.96f blue:0.07f alpha:1.00f]
+[UIColor colorWithRed:0.96f green:0.96f blue:0.07f alpha:1.00f],
+[UIColor colorWithRed:0.25f green:0.25f blue:0.01f alpha:1.00f],
+[UIColor colorWithRed:0.80f green:0.91f blue:0.86f alpha:1.00f],
+[UIColor colorWithRed:0.00f green:0.53f blue:0.31f alpha:1.00f],
+[UIColor colorWithRed:0.02f green:0.29f blue:0.18f alpha:1.00f],
+[UIColor colorWithRed:0.90f green:0.91f blue:0.91f alpha:1.00f],
+[UIColor colorWithRed:0.40f green:0.40f blue:0.40f alpha:1.00f],
+[UIColor colorWithRed:0.22f green:0.22f blue:0.22f alpha:1.00f],
+[UIColor colorWithRed:1.00f green:0.79f blue:0.05f alpha:1.00f],
+[UIColor colorWithRed:0.40f green:0.18f blue:0.62f alpha:1.00f]
     ];
   });
 
